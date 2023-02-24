56adc7550647b969e16fa1f43c8507690f5006d5
Hanwen Wang <hanwenwang@Hanwens-MacBook-Pro.local>
2023-02-24 12:20:07 -0800

    shinyapp v0. file created

---
+++ hw3/mimiciv_shiny/shinyapp.R
@@ -0,0 +1,20 @@
+# "Biostat 203B Homework 3" - Shiny app
+# Due Mar 1 @ 11:59PM
+# author: Sylvia Wang 105118268
+
+sessionInfo()
+
+# app.R
+library(shiny)
+library(tidyverse)
+
+icu_cohort <- readRDS("./mimiciv_shiny/icu_cohort.rds")
+
+
+
+
+
+
+
+
+
